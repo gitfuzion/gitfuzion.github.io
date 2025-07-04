9d49d7288ef3c2ed772952c4343782971da351b5
gitjalm <fuzion5m12@gmail.com>
2025-07-04 20:10:30 +0200

    Initial theme fixed for FzStudio

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,74 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from 'react';
+import { DocsThemeConfig, useConfig } from 'nextra-theme-docs';
+import { useRouter } from 'next/router';
 
-const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
+function useHead() {
+  const { asPath } = useRouter();
+  const { frontMatter, title } = useConfig();
+  const url = `https://gitfuzion.github.io${asPath}`;
+  const description = frontMatter.description || "Documentation for FzStudio assets";
+
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" type="image/x-icon" href="/static/ox.ico" />
+      <meta httpEquiv="Content-Language" content="en" />
+      <meta name="description" content={description} />
+      <meta name="og:title" content={title} />
+      <meta name="og:description" content={description} />
+      <meta name="og:url" content={url} />
+    </>
+  );
 }
 
-export default config
+function useNextSeoProps() {
+  const { asPath } = useRouter();
+  const arr = asPath.replace(/[-_]/g, ' ').split('/');
+  const category = (arr[1][0] !== '#' && arr[1]) || 'FzStudio';
+  const rawTitle = arr[arr.length - 1];
+  const title = /[a-z]/.test(rawTitle) && /[A-Z]/.test(rawTitle) ? rawTitle : '%s';
+
+  return {
+    titleTemplate: `${title} - ${
+      rawTitle === category ? 'Documentation' : category.replace(/(^\w|\s\w)/g, (m) => m.toUpperCase())
+    }`,
+  };
+}
+
+const config: DocsThemeConfig = {
+  logo: (
+    <div
+      style={{
+        paddingLeft: '50px',
+        lineHeight: '38px',
+        background: "url(/fz-studio-logo.png) no-repeat left",
+        backgroundSize: '38px',
+        fontWeight: 550,
+      }}
+    >
+      FzStudio
+    </div>
+  ),
+  project: {
+    link: 'https://github.com/gitfuzion',
+  },
+  chat: {
+    link: 'https://discord.gg/yk5ean6rte',
+  },
+  docsRepositoryBase: 'https://github.com/gitfuzion/gitfuzion.github.io/blob/main',
+  footer: {
+    text: 'FzStudio Documentation',
+  },
+  head: useHead,
+  primaryHue: { dark: 200, light: 200 },
+  sidebar: {
+    defaultMenuCollapseLevel: 1,
+  },
+  toc: {
+    backToTop: true,
+  },
+
+  useNextSeoProps: useNextSeoProps,
+};
+
+export default config;
